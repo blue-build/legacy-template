bca6a014dbe400e2688df04693b22343f367b3bb
Benjamin Manuel <benjamin.r.manuel@gmail.com>
2023-05-08 13:14:40 -0500

    Keep rpm firefox (#4)

--- build.sh
+++ build.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 # run scripts
 echo "-- Running scripts defined in recipe.yml --"
-buildscripts=$(yq '.scripts[]' < /usr/etc/ublue-recipe.yml)
-for script in $(echo -e "$buildscripts"); do \
-    echo "Running: ${script}" && \
-    /tmp/scripts/$script; \
+buildscripts=$(yq '.scripts[]' </usr/etc/ublue-recipe.yml)
+for script in $(echo -e "$buildscripts"); do
+    echo "Running: ${script}" &&
+        /tmp/scripts/$script
 done
 echo "---"
 
 # remove the default firefox (from fedora) in favor of the flatpak
-rpm-ostree override remove firefox firefox-langpacks
+# rpm-ostree override remove firefox firefox-langpacks
 
-repos=$(yq '.extrarepos[]' < /usr/etc/ublue-recipe.yml)
+repos=$(yq '.extrarepos[]' </usr/etc/ublue-recipe.yml)
 if [[ -n "$repos" ]]; then
     echo "-- Adding repos defined in recipe.yml --"
-    for repo in $(echo -e "$repos"); do \
-        wget $repo -P /etc/yum.repos.d/; \
+    for repo in $(echo -e "$repos"); do
+        wget $repo -P /etc/yum.repos.d/
     done
     echo "---"
 fi
 
 echo "-- Installing RPMs defined in recipe.yml --"
-rpm_packages=$(yq '.rpms[]' < /usr/etc/ublue-recipe.yml)
-for pkg in $(echo -e "$rpm_packages"); do \
-    echo "Installing: ${pkg}" && \
-    rpm-ostree install $pkg; \
+rpm_packages=$(yq '.rpms[]' </usr/etc/ublue-recipe.yml)
+for pkg in $(echo -e "$rpm_packages"); do
+    echo "Installing: ${pkg}" &&
+        rpm-ostree install $pkg
 done
 echo "---"
 
@@ -32,12 +32,12 @@ echo "---"
 pip install --prefix=/usr yafti
 
 # add a package group for yafti using the packages defined in recipe.yml
-flatpaks=$(yq '.flatpaks[]' < /tmp/ublue-recipe.yml)
+flatpaks=$(yq '.flatpaks[]' </tmp/ublue-recipe.yml)
 # only try to create package group if some flatpaks are defined
-if [[ -n "$flatpaks" ]]; then            
+if [[ -n "$flatpaks" ]]; then
     yq -i '.screens.applications.values.groups.Custom.description = "Flatpaks defined by the image maintainer"' /usr/etc/yafti.yml
     yq -i '.screens.applications.values.groups.Custom.default = true' /usr/etc/yafti.yml
-    for pkg in $(echo -e "$flatpaks"); do \
+    for pkg in $(echo -e "$flatpaks"); do
         yq -i ".screens.applications.values.groups.Custom.packages += [{\"$pkg\": \"$pkg\"}]" /usr/etc/yafti.yml
     done
 fi
