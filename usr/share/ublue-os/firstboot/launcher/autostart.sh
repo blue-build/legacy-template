fc1f35331f76eb0d2c5f14f831453f9a81ffd98e
apricotant <fronkryan@gmail.com>
2023-07-19 21:58:14 -0400

    Update autostart.sh

--- usr/share/ublue-os/firstboot/launcher/autostart.sh
+++ usr/share/ublue-os/firstboot/launcher/autostart.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # Simply launches the "yafti" GUI with the uBlue image's configuration.
-/usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
+#/usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
+
+just setup-flatpaks
