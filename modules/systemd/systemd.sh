cda9e8aa7cea941142980646ac7c5a5195f79c42
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-09-03 09:11:27 -0700

    chore: remove debug config and code from systemd module

--- modules/systemd/systemd.sh
+++ modules/systemd/systemd.sh
@@ -12,32 +12,24 @@ get_yaml_array USER_DISABLED '.user.disabled[]' "$1"
 if [[ ${#ENABLED[@]} -gt 0 ]]; then
     for unit in "${ENABLED[@]}"; do
         unit=$(printf "$unit")
-        echo "$unit"
-        systemctl list-unit-files | grep $unit
         systemctl enable $unit
     done
 fi
 if [[ ${#DISABLED[@]} -gt 0 ]]; then
     for unit in "${DISABLED[@]}"; do
         unit=$(printf "$unit")
-        echo "$unit"
-        systemctl list-unit-files | grep $unit
         systemctl disable $unit
     done
 fi
 if [[ ${#USER_ENABLED[@]} -gt 0 ]]; then
     for unit in "${ENABLED[@]}"; do
         unit=$(printf "$unit")
-        systemctl list-unit-files | grep $unit
-        echo "$unit"
         systemctl --user enable $unit
     done
 fi
 if [[ ${#USER_DISABLED[@]} -gt 0 ]]; then
     for unit in "${DISABLED[@]}"; do
         unit=$(printf "$unit")
-        echo "$unit"
-        systemctl list-unit-files | grep $unit
         systemctl --user disable $unit
     done
 fi
