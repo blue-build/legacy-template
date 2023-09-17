b8e579ff295c9941c5ed9ef4d62277b62e9ffa60
asyrafng <145214775+asyrafng@users.noreply.github.com>
2023-09-17 17:44:50 +0700

    override remove command

--- config/scripts/rpmfusion.sh
+++ config/scripts/rpmfusion.sh
@@ -10,6 +10,8 @@ rpm-ostree install \
     /tmp/rpms/*.rpm \
     fedora-repos-archive
 
-rpm-ostree install \
-    ffmpeg ffmpeg-libs ffmpegthumbnailer intel-media-driver libva-intel-driver \
+rpm-ostree override remove \
+    libavcodec-free libavfilter-free libavformat-free libavutil-free \
+    libpostproc-free libswresample-free libswscale-free mesa-va-drivers google-noto-sans-cjk-vf-fonts \
+    --install=ffmpeg ffmpeg-libs ffmpegthumbnailer intel-media-driver libva-intel-driver \
     mesa-va-drivers-freeworld pipewire-codec-aptx
