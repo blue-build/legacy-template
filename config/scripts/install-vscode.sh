96e6c48f0a891ffedc20cc4fb3064addec3c73ec
Dennis <github@tutan.de>
2023-09-17 23:24:01 +0200

    Update dconf and update packages

--- config/scripts/install-vscode.sh
+++ config/scripts/install-vscode.sh
@@ -8,3 +8,16 @@ set -oue pipefail
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 rpm-ostree refresh-md
 rpm-ostree install code
+
+# Install extensions
+declare -a extensions=(
+  "ms-python.python"
+  "charliermarsh.ruff"
+  "ms-python.isort"
+  "ms-python.black-formatter"
+  "ms-vscode-remote.remote-containers"
+  "redhat.vscode-yaml"
+)
+for ext in "${extensions[@]}"; do
+  code --install-extension $ext
+done
