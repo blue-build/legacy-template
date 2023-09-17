3030f9d295cc35f7ef5c47fc0b0de72b519a725b
asyrafng <145214775+asyrafng@users.noreply.github.com>
2023-09-17 17:19:08 +0700

    Create rpmfusion.sh

---
+++ config/scripts/rpmfusion.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -ouex pipefail
+
+wget -P /tmp/rpms \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
+
+rpm-ostree install \
+    /tmp/rpms/*.rpm \
+    fedora-repos-archive
