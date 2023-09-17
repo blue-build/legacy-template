a4573ed364d8d855de03786acba3191f28af91f0
Dennis <github@tutan.de>
2023-09-17 17:44:58 +0200

    Update installed packages, dconf and extra post-install scripts

---
+++ config/scripts/install-chezmoi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# https://techviewleo.com/install-wezterm-on-fedora-opensuse/?expand_article=1
+VER=$(curl -s https://api.github.com/repos/twpayne/chezmoi/releases/latest|grep tag_name|cut -d '"' -f 4)
+VER2=$(echo "$VER" | sed -r 's/v//g')
+rpm-ostree install https://github.com/twpayne/chezmoi/releases/download/${VER}/chezmoi-${VER2}-x86_64.rpm
