6bc6dea5398984b4ae7e294ac47445a4dcf7261e
asyrafng <145214775+asyrafng@users.noreply.github.com>
2023-09-17 21:18:23 +0700

    Update rpmfusion.sh

--- config/scripts/rpmfusion.sh
+++ config/scripts/rpmfusion.sh
@@ -13,5 +13,6 @@ rpm-ostree install \
 rpm-ostree override remove \
     libavcodec-free libavfilter-free libavformat-free libavutil-free \
     libpostproc-free libswresample-free libswscale-free mesa-va-drivers google-noto-sans-cjk-vf-fonts \
-    --install=ffmpeg ffmpeg-libs ffmpegthumbnailer intel-media-driver libva-intel-driver \
+
+rpm-ostree install ffmpeg ffmpeg-libs ffmpegthumbnailer intel-media-driver libva-intel-driver \
     mesa-va-drivers-freeworld pipewire-codec-aptx
