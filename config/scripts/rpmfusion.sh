18bece784af263573e27dc33ed6d7cc7a203118f
asyrafng <145214775+asyrafng@users.noreply.github.com>
2023-09-17 21:03:34 +0700

    Update rpmfusion.sh

--- config/scripts/rpmfusion.sh
+++ config/scripts/rpmfusion.sh
@@ -3,8 +3,8 @@
 set -ouex pipefail
 
 wget -P /tmp/rpms \
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-38.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-38.noarch.rpm
 
 rpm-ostree install \
     /tmp/rpms/*.rpm \
