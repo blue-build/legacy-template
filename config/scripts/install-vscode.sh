07574cb78dcf30f84797b6f40abe61b97a08d185
Dennis <github@tutan.de>
2023-09-21 20:55:03 +0200

    Add shells

--- config/scripts/install-vscode.sh
+++ config/scripts/install-vscode.sh
@@ -5,19 +5,31 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-rpm-ostree refresh-md
-rpm-ostree install code
+rpm -q code > /dev/null
+if [ $? -eq 1 ]; then
+    echo "Installing Visual Studio Code as an overlay package..."
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    rpm-ostree refresh-md
+    rpm-ostree install code
+    echo "Please reboot to finish installation of the layered package."
+    exit 0
+else
+    echo "Visual Studio Code overlay is already installed."
+    # Install extensions
+    echo "Installing Visual Studio Code extensions..."
 
-# Install extensions
-declare -a extensions=(
-  "ms-python.python"
-  "charliermarsh.ruff"
-  "ms-python.isort"
-  "ms-python.black-formatter"
-  "ms-vscode-remote.remote-containers"
-  "redhat.vscode-yaml"
-)
-for ext in "${extensions[@]}"; do
-  code --install-extension $ext
-done
+    declare -a extensions=(
+    "ms-python.python"
+    "charliermarsh.ruff"
+    "ms-python.isort"
+    "ms-python.black-formatter"
+    "ms-vscode-remote.remote-containers"
+    "redhat.vscode-yaml"
+    )
+    for ext in "${extensions[@]}"; do
+    code --install-extension $ext
+    done
+fi
+
+
+
