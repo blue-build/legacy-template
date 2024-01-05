8807734de09eb1a31e6d76884ec553157fa22686
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2024-01-05 20:14:39 +0000

    Theoretically installing mullvad now, lets see...

---
+++ config/scripts/mullvad.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+wget -O /tmp/mullvad.rpm https://mullvad.net/en/download/app/rpm/latest
+rpm-ostree install /tmp/mullvad.rpm
+systemctl enable --now mullvad-daemon
