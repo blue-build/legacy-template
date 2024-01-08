b1003ae89d913a0d47af9efe6e53c35b7c4a1ee0
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2024-01-08 10:51:10 +0000

    Testing plymouth themes again

---
+++ config/scripts/plymouth.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+plymouth-set-default-theme -R abstract_ring
