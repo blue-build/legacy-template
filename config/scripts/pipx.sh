9794039db6c2a310a9674a243ca2ef5fc14da27d
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2024-01-09 16:08:04 +0000

    Added pipx install script, change to custom module maybe?

---
+++ config/scripts/pipx.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+while IFS= read -r pkg; do echo "Installing $pkg" && PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin pipx install $pkg; done < $CONFIG_DIRECTORY/scripts/packages/pipx.pkg
