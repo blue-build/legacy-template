a644486c9ef09c5fe1982e11bf35fe7d71ef9b81
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2024-01-12 23:45:39 +0000

    Moved pipx install to yafti

--- config/scripts/pipx.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-# Your code goes here.
-while IFS= read -r pkg; do echo "Installing $pkg" && PIPX_HOME=/usr/lib/opt/pipx PIPX_BIN_DIR=/var/usrlocal/bin pipx install $pkg; done < $CONFIG_DIRECTORY/scripts/packages/pipx.pkg
