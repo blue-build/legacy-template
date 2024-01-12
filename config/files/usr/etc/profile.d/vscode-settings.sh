b705ee85f34080fac21ca39adb9cedefd0524e8a
FlyinPancake <36113055+FlyinPancake@users.noreply.github.com>
2024-01-12 09:49:43 +0000

    feat: added vscode

---
+++ config/files/usr/etc/profile.d/vscode-settings.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if test "$(id -u)" -gt "0" && test -d "$HOME"; then
+    # Add default settings when there are no settings
+    if test ! -e "$HOME"/.config/Code/User/settings.json; then
+        mkdir -p "$HOME"/.config/Code/User
+        cp -f /etc/skel.d/.config/Code/User/settings.json "$HOME"/.config/Code/User/settings.json
+    fi
+fi
